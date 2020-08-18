7f3507806d80e83c4f788aa214f33ff7938432bb
Matheus Navarro <matheusnavarro@alu.ufc.br>
2020-08-18 08:09:31 -0300

    [+] juntando soma e subt

--- DesafioCalculadora/Operacao.java
+++ DesafioCalculadora/Operacao.java
@@ -1,3 +1,23 @@
 abstract class Operacao {
 	abstract Operando operar(Operando a, Operando b);
 }
+
+class Soma extends Operacao {
+	Operando operar(Operando a, Operando b){
+		int v1 = a.getValor();
+		int v2 = b.getValor();
+		int resposta = v1 + v2;
+		Operando resultado = new Operando(resposta); 
+		return (resultado);
+	}
+}
+
+class Subt extends Operacao {
+	Operando operar(Operando a, Operando b){
+		int v1 = a.getValor();
+		int v2 = b.getValor();
+		int resposta = v1 - v2;
+		Operando resultado = new Operando(resposta); 
+		return (resultado);
+	}
+}
--- DesafioCalculadora/Subt.java
+++
@@ -1,6 +0,0 @@
-public class Subt extends Operacao {
-	Operando operar(Operando a, Operando b){
-		Operando resultado = new Operando(a.getValor() - b.getValor());
-		return resultado;
-	}
-}
